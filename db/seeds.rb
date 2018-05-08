23d6556af0b8d11a2d27c88315a2f4c8d03f2aa7
annie chiang <annie100patty@gmail.com>
2018-05-08 20:07:01 +0900

    setup seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Category
+
+Category.destroy_all
+
+category_list = [
+  { name: "商業"},
+  { name: "科技" },
+  { name: "心理" },
+  { name: "美食" },
+  { name: "旅遊" },
+  { name: "生活" },
+  { name: "新奇" }
+]
+
+category_list.each do |category|
+  Category.create( name: category[:name] )
+end
+
+puts "Category created!"
+
+# Default admin
+
+User.create(name: "Admin", email: "admin@example.com", password: "12345678", role: "admin")
+puts "Default admin created!"
