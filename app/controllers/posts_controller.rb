f589404acf3b50da3f93d19eb2aea67489f138be
annie chiang <annie100patty@gmail.com>
2018-05-09 15:49:57 +0900

    add edit action for post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,28 @@
 class PostsController < ApplicationController
+
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
+
   def index
     @posts = Post.all.page(params[:page]).per(20)
   end
 
-  def show
+  def update
+    if @post.update(post_params)
+      flash[:notice] = "Successfully updated"
+      redirect_to post_path(@post)
+    else
+      flash[:notice] = "Failed to update"
+      render :edit 
+    end
+  end
+
+  private
+
+  def set_post
     @post = Post.find(params[:id])
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :content, :photo)
   end
 end
