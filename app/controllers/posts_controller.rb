3b46ee97da4978f7ab0006b20a2349d5ae12e620
annie chiang <annie100patty@gmail.com>
2018-05-09 16:09:09 +0900

    add edit and destroy action for post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,6 +6,22 @@ class PostsController < ApplicationController
     @posts = Post.all.page(params[:page]).per(20)
   end
 
+  def new 
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.user = current_user
+    if @post.save
+      flash[:notice] = "Post was successfully created"
+      redirect_to posts_path
+    else
+      flash[:notice] = "Post was failed to create"
+      render :new 
+    end
+  end
+
   def update
     if @post.update(post_params)
       flash[:notice] = "Successfully updated"
@@ -16,6 +32,11 @@ class PostsController < ApplicationController
     end
   end
 
+  def destroy
+    @post.destroy
+    redirect_to posts_path
+  end
+
   private
 
   def set_post
@@ -23,6 +44,6 @@ class PostsController < ApplicationController
   end
 
   def post_params
-    params.require(:post).permit(:title, :content, :photo)
+    params.require(:post).permit(:title, :content, :photo, :category_id)
   end
 end
