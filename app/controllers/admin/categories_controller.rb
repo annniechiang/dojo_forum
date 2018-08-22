363f118b75e88de143ec92c3b0795db51413998a
annie chiang <annie100patty@gmail.com>
2018-08-22 22:38:51 +0800

    complete admin category index

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,5 +1,55 @@
 class Admin::CategoriesController < ApplicationController
+
+  before_action :set_category, only: [:update, :destroy]
+
   def index
     @categories = Category.all
+
+    if params[:id]
+      set_category
+    else
+      @category = Category.new
+    end
   end
+
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      flash[:notice] = "category was successfully created"
+      redirect_to admin_categories_path
+
+    else
+      @categories = Category.all
+      render :index
+    end
+  end
+
+  def update
+    if @category.update(category_params)
+      redirect_to admin_categories_path
+      flash[:notice] = "category was successfully updated"
+    else
+      @categories = Category.all
+      render :index
+    end
+  end
+
+  def destroy
+    if @category.destroy
+      flash[:alert] = "category was successfully deleted"
+    else
+      flash[:alert] = @category.errors.full_messages.to_sentence
+    end
+    redirect_to admin_categories_path
+  end
+
+  private
+
+  def set_category
+    @category = Category.find(params[:id])
+  end
+
+  def category_params
+    params.require(:category).permit(:name)
+  end 
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,3 @@
 class Category < ApplicationRecord
-  has_many :posts
+  has_many :posts, dependent: :restrict_with_error
 end
