b3617cabf8db63fbc69c72b848b9d252109ef28d
annie chiang <annie100patty@gmail.com>
2018-08-27 21:15:20 +0800

    can change role in admin user index

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -2,4 +2,18 @@ class Admin::UsersController < ApplicationController
   def index 
     @users = User.all
   end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to admin_users_path
+      flash[:notice] = "user was successfully updated"
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:role)
+  end
 end
