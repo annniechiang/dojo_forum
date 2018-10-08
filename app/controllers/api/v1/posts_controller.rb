41f09adeba26afd78c3b3c52eeb3f774dd51d444
annie chiang <annie100patty@gmail.com>
2018-10-08 20:11:43 +0800

    add index and show actions for post api

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -1,2 +1,22 @@
 class Api::V1::PostsController < ApiController
+  def index
+    @posts = Post.all
+    render json: {
+      data: @posts
+    }
+  end
+
+  def show
+    @post = Post.find_by(id: params[:id])
+    if !@post
+      render json: {
+        message: "Can't find the post!",
+        status: 400
+      }
+    else
+      render json: {
+        data: @post
+      }
+    end
+  end
 end
