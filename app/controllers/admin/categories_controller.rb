01d48e8cea809fb37d156eb5f68f017f021a5f81
annie chiang <annie100patty@gmail.com>
2018-10-08 22:34:58 +0800

    modify authority in admin pages and post

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,4 +1,4 @@
-class Admin::CategoriesController < ApplicationController
+class Admin::CategoriesController < Admin::BaseController
 
   before_action :set_category, only: [:update, :destroy]
 
--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -4,7 +4,6 @@ class ApiController < ActionController::Base
   def authenticate_user_from_token!
     if params[:auth_token].present?
       user = User.find_by_authentication_token(params[:auth_token])
-      # sign_in 是 Devise 的方法
       sign_in(user, store: false) if user
     end
   end
