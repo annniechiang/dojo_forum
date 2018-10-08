c2e2e8710135a7fc4ba81bef767c1ebfc79f3610
annie chiang <annie100patty@gmail.com>
2018-10-08 22:11:35 +0800

    add login and logout action in api

---
+++ app/controllers/api/v1/auth_controller.rb
@@ -0,0 +1,25 @@
+class Api::V1::AuthController < ApiController
+  before_action :authenticate_user!, only: :logout
+
+  def login
+    if params[:email] && params[:password]
+      user = User.find_by_email(params[:email])
+    end
+
+    if user && user.valid_password?(params[:password])
+      render json: {
+        message: "Ok",
+        auth_token: user.authentication_token,
+        user_id: user.id}
+    else
+      render json: { message:  "Email or Password is wrong" }, status:  401
+    end
+  end
+
+  def logout
+    current_user.generate_authentication_token
+    current_user.save!
+
+    render json: { message:  "Ok" }
+  end
+end
